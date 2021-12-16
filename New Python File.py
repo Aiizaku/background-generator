5f3c3eecc460521019f2129160575bf927b84b47
Isaac Brown <aiizaku@Live.com>
2021-12-16 11:08:18 -0500

    added input and age computation

--- New Python File.py
+++ New Python File.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Hello Macy")
+name = input("Please share your name.")
+age = int(input("please share how old you are."))
+
+if age > 18:
+    print("You are an adult")
+elif age >= 13:
+    print("You are a teenager")
+else:
+    print("You are a child")
